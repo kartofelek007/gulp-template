538f049bc687acaeaa1c4fe1fcb2864647b664da
Marcin Domański <kartofelek007@gmail.com>
2017-08-19 21:40:07 +0200

    poprawka w webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,25 @@
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
-    entry: "./src/js/app.js",
-    output: { filename: "./dist/js/script.min.js" },
+    entry: './src/js/app.js',
+    output: { 
+        path: `${__dirname}/dist/js`,
+        filename: 'script.min.js' 
+    },
     watch: false, //mozna pominac
     devtool: "source-map",
     module: {
         loaders: [
             {
-                test: /\.js$/,  exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: { presets: ['es2015'] }
+                test: /\.js$/,  
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: { 
+                        presets: ['es2015'] 
+                    }
+                }
+                
             }
         ],        
     },
