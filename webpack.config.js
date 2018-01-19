ec3c956b6dae6e4d21a49dafc5d45c0d31a53c28
Marcin Domański <kartofelek007@gmail.com>
2018-01-19 12:40:30 +0100

    poprawka w webpack - uzycie nowszego presetu

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,28 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
     entry: './src/js/app.js',
-    output: { 
+    output: {
         path: `${__dirname}/dist/js`,
-        filename: 'script.min.js' 
+        filename: 'script.min.js'
     },
     watch: false, //mozna pominac
     devtool: "source-map",
     module: {
         loaders: [
             {
-                test: /\.js$/,  
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: { 
-                        presets: ['es2015'] 
-                    }
+                test: /\.js$/,  exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    "presets": [
+                        ["env", {
+                          "targets": {
+                            "browsers": ["> 1%"]
+                          }
+                        }]
+                      ]
                 }
-                
             }
-        ],        
+        ],
     },
     plugins: [
         new UglifyJSPlugin({
